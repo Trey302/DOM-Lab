03b1aa904be3e17528ad2665b3e31b7c71d3f543
Route9Library <you@example.com>
2020-12-07 16:00:40 -0500

    my own info

---
+++ main.js
@@ -0,0 +1,5 @@
+document.body.style.fontFamily = 'Arial, sans-serif';
+document.getElementById('Nickname').innerHTML = 'Trey Trey';
+document.getElementById('Favorites').innerHTML = 'Rap, Wings, Lost';
+document.getElementById('Hometown').innerHTML = 'Newark, DE';
+document.getElementById('Fact').innerHTML = 'Im right handed';
